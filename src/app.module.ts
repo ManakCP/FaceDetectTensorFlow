7c976f4451c10cdb85be17606e2c55855a2e701a
ManakCP <manakchand@outlook.com>
2017-10-22 13:32:34 +0530

    Added JWT Token authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import {Module} from '@nestjs/common';
+import {Module,NestModule,MiddlewaresConsumer,RequestMethod} from '@nestjs/common';
 import {companiesController} from './companies.controller';
+import {AuthenticationMiddleware} from './authentication.middleware';
 
 @Module({
     modules:[],
     controllers:[companiesController]
 })
 
-export class ApplicationModule { }
+export class ApplicationModule { 
+    /* 
+    //JWT Implementation in progress.
+    //Need to create 0Auth account to generate Authorisation Key.
+    configure = (consumer: MiddlewaresConsumer): void => {
+        consumer.apply(AuthenticationMiddleware).forRoutes(
+            {path:'/**', method: RequestMethod.ALL}
+        );
+    }*/
+}
