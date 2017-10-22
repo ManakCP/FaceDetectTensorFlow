35c8963b1fca2be0bff384889460cd37506ce27f
ManakCP <manakchand@outlook.com>
2017-10-22 13:39:27 +0530

    added Distribution folder in project

---
+++ dist/company.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Company {
+    constructor(name, industry, address) {
+        this.name = name;
+        this.industry = industry;
+        this.address = address;
+    }
+}
+exports.Company = Company;
+//# sourceMappingURL=company.js.map
